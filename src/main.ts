d977ace172a588a3d4fe9cfd4fb288771829c1ad
kitori <1328798322@qq.com>
2023-05-02 17:31:12 +0800

    初始化项目

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import 'normalize.css'
 import './assets/css/index.less'
 import App from './App.vue'
 import router from './router'
+import pinia from './stores'
 
 const app = createApp(App)
 
-app.use(router).mount('#app')
+app.use(router).use(pinia).mount('#app')
--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "xxx";
-export const TIME_OUT = 10000;
+export const BASE_URL = 'xxx'
+export const TIME_OUT = 10000
